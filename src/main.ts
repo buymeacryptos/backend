8998f30b8471f93b3516f41bb0a392a334becd0b
Hook Tools <96314649+monleru@users.noreply.github.com>
2023-08-12 19:00:13 +0600

    4444

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(4444);
 }
 bootstrap();
